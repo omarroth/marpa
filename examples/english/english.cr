fb55f6603854d42f78a41677d41374e53efe8c5a
Omar Roth <omarroth@hotmail.com>
2018-06-04 17:30:37 -0500

    Fix typo

--- examples/english/english.cr
+++ examples/english/english.cr
@@ -5,7 +5,7 @@ grammar = File.read("english.bnf")
 input = File.read("sample.english")
 
 OptionParser.parse! do |parser|
-  parser.banner = "Usage: marpa [arguments]"
+  parser.banner = "Usage: english [arguments]"
   parser.on("-i FILE", "--input=FILE", "Input file") { |file| input = File.read(file) }
   parser.on("-p STRING", "--puts=STRING", "Input string") { |string| input = string }
   parser.on("-h", "--help", "Show this help") { puts parser }
