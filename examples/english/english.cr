3532e61a645e31d7f759e61c2e85d70d6ae6111d
Omar Roth <omarroth@hotmail.com>
2018-06-04 17:25:57 -0500

    Add english example

---
+++ examples/english/english.cr
@@ -0,0 +1,22 @@
+require "../../src/marpa.cr"
+require "option_parser"
+
+grammar = File.read("english.bnf")
+input = File.read("sample.english")
+
+OptionParser.parse! do |parser|
+  parser.banner = "Usage: marpa [arguments]"
+  parser.on("-i FILE", "--input=FILE", "Input file") { |file| input = File.read(file) }
+  parser.on("-p STRING", "--puts=STRING", "Input string") { |string| input = string }
+  parser.on("-h", "--help", "Show this help") { puts parser }
+end
+
+stack = parse(grammar, input, tag = true)
+stack = stack.as(Array(RecArray))
+stack.each do |sentence|
+  sentence = sentence.as(Array(RecArray))
+  sentence = sentence.flatten
+  sentence = sentence.join(" ")
+
+  puts sentence
+end
--- src/marpa.cr
+++ src/marpa.cr
@@ -1,8 +1,5 @@
 require "./helpers"
 require "./lib_marpa"
-require "json"
-require "math"
-require "option_parser"
 
 alias Rule = Hash(String, String | Array(String))
 alias RecArray = Array(RecArray) | String
@@ -451,24 +448,3 @@ def compile_grammar(input)
   puts "]"
 end
 
-grammar = File.read("src/bnf/english.bnf")
-# input = File.read("src/sample.english")
-
-# OptionParser.parse! do |parser|
-#   parser.banner = "Usage: marpa [arguments]"
-#   parser.on("-i FILE", "--input=FILE", "Input file") { |file| input = File.read(file) }
-#   parser.on("-h", "--help", "Show this help") { puts parser }
-# end
-
-# print "Input sentence: "
-input = File.read("src/simple.english")
-
-stack = parse(grammar, input, tag = true)
-stack = stack.as(Array(RecArray))
-stack.each do |sentence|
-  sentence = sentence.as(Array(RecArray))
-  sentence = sentence.flatten
-  sentence = sentence.join(" ")
-
-  puts sentence
-end
