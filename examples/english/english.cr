44b0d1a323dd45bf0e06d3a99a70f4987fd9752e
Omar Roth <omarroth@hotmail.com>
2018-06-05 12:22:55 -0500

    Exit examples when given -h flag

--- examples/english/english.cr
+++ examples/english/english.cr
@@ -13,7 +13,10 @@ OptionParser.parse! do |parser|
   parser.banner = "Usage: english [arguments]"
   parser.on("-i FILE", "--input=FILE", "Input file") { |file| input = File.read(file) }
   parser.on("-p STRING", "--puts=STRING", "Input string") { |string| input = string }
-  parser.on("-h", "--help", "Show this help") { puts parser }
+  parser.on("-h", "--help", "Show this help") do
+    puts parser
+    exit
+  end
 end
 
 parser = Marpa::Parser.new
